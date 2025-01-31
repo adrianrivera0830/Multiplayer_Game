70b47148cfa1bec0ab83defe80b4f236aa463f8e
Adrian Rivera <adrianluna0830@gmail.com>
2025-01-30 22:59:42 -0600

    initialize code on main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+#include "NetworkHeaders.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+#ifdef _WIN32
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        std::cerr << "WSAStartup failed: " << result << std::endl;
+        return 1;
+    }
+#endif
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
     return 0;
 }
