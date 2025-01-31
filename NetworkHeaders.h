258a633674a3bc1801f36dd1fb87357a4187e095
Adrian Rivera <adrianluna0830@gmail.com>
2025-01-30 22:56:17 -0600

    add networkHeaders file and definitions

---
+++ NetworkHeaders.h
@@ -0,0 +1,43 @@
+//
+// Created by Cuent on 1/30/2025.
+//
+
+#ifndef NETWORKHEADERS_H
+#define NETWORKHEADERS_H
+
+#pragma once
+
+#if defined(_WIN32)
+    #ifndef _WIN32_WINNT
+        #define _WIN32_WINNT 0x0600
+    #endif
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")
+#else
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <netdb.h>
+    #include <unistd.h>
+    #include <errno.h>
+#endif
+
+#include <iostream>
+#include <string.h>
+
+#if defined(_WIN32)
+    #define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+    #define CLOSESOCKET(s) closesocket(s)
+    #define GETSOCKETERRNO() (WSAGetLastError())
+    typedef int socklen_t;
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#define SOCKET_ERROR -1
+#endif
+
+#endif //NETWORKHEADERS_H
